32729e230ba147273fd2e31a1d5f703dbda042e5
mentzer04 <136191044+mentzer04@users.noreply.github.com>
2023-07-22 17:29:46 +0400

    Add files via upload

---
+++ Программа графический редактор/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public string filename; // название файла
+        public Form1()
+        {
+            InitializeComponent();
+            SetSize();
+        }
+        private class ArrayPoints
+        {
+            private int index = 0;
+            private Point[] points;
+
+            public ArrayPoints(int size)
+            {
+                if (size <= 0)
+                {
+                    size = 2;
+                }
+                points = new Point[size];
+            }
+
+            public void SetPoint(int x, int y)
+            {
+                if (index >= points.Length)
+                {
+                    index = 0;
+                }
+                points[index] = new Point(x, y);
+                index++;
+            }
+
+            public void ResetPoints()
+            {
+                index = 0;
+            }
+
+            public int GetCountPoints()
+            {
+                return index;
+            }
+
+            public Point[] GetPoints()
+            {
+                return points;
+            }
+        }
+
+        private bool isMouse = false;
+        private ArrayPoints arrayPoints = new ArrayPoints(2);
+
+        Bitmap map = new Bitmap(100, 100);
+        Graphics graphics;
+
+        Pen pen = new Pen(Color.Black, 3f);
+
+        private void SetSize()
+        {
+            Rectangle rectangle = Screen.PrimaryScreen.Bounds;
+            map = new Bitmap(rectangle.Width, rectangle.Height);
+            graphics = Graphics.FromImage(map);
+
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouse = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouse = false;
+            arrayPoints.ResetPoints();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isMouse)
+            {
+                return;
+            }
+            arrayPoints.SetPoint(e.X, e.Y);
+            if (arrayPoints.GetCountPoints() >= 2)
+            {
+                graphics.DrawLines(pen,arrayPoints.GetPoints());
+                pictureBox1.Image = map;
+                arrayPoints.SetPoint(e.X, e.Y);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pen.Color = ((Button)sender).BackColor;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = map;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "JPG(*.JPG)|*.jpg";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (pictureBox1.Image != null)
+                {
+                    pictureBox1.Image.Save(saveFileDialog1.FileName);
+                }
+            }
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            pen.Width = trackBar1.Value;
+        }
+        private void OpenClick(object sender, EventArgs e) // открыть файл, кнопка
+        {
+
+            //SaveUnsavedFile(); // вызов функции, отвечающей за сохранение несохраненного документа
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) // условие для открытия файла
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(openFileDialog1.FileName); // открытие чтения документа
+                    string a = openFileDialog1.FileName.Replace("\\" , "\\" );
+                    pictureBox1.Image = Image.FromFile(a);
+                    sr.Close(); // закрытие документа
+                    filename = openFileDialog1.FileName; // текущий документ принимает имя открываемого ранее документа
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось открыть файл!"); // сообщение об ошибке
+                }
+            }
+            //UpdateTextWithTitle(); // вызов функции проверяющей изменилось ли название документа
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
